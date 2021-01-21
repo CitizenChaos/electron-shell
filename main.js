9dd93b069becede57c687e008a89488d5b65960b
王阳 <785064576@qq.com>
2021-01-21 13:48:10 +0800

    feat: init

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
+const path = require('path')
+function createWindow() {
+  const win = new BrowserWindow({
+    // width: 800,
+    // height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false
+    }
+  })
+
+  win.maximize()
+  // Menu.setApplicationMenu(null)
+  // win.webContents.openDevTools()
+  process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true
+
+  // win.loadURL('https://test-vshow.fooww.com/group/')
+  win.loadURL('http://localhost:8081/group/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+ipcMain.on('openPC', (event, arg) => {
+  console.log(arg)
+  let clientPath = 'D:\\梵讯房屋管理系统\\FoowwSoft.exe -' + arg
+  shell.openPath(path.join(clientPath))
+})
