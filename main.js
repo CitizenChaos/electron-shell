be43931bd1ee60ef21e8e388dbff37d185e0b91f
王阳 <785064576@qq.com>
2021-01-27 13:11:33 +0800

    feat: requestSingleInstanceLock

--- main.js
+++ main.js
@@ -2,6 +2,31 @@ const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const { getSoftwarePath } = require('./getSoftwarePath')
 const { spawn } = require('child_process')
 const { networkInterfaces } = require('os')
+const axios = require('axios')
+var currentLoginUserId = 0
+var currentLoginId = 0
+
+let win = null
+
+// ensure app only one instance
+const getTheLock = app.requestSingleInstanceLock()
+if (!getTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // 当运行第二个实例时,将会聚焦到myWindow这个窗口
+    if (win) {
+      if (win.isMinimized()) win.restore()
+      win.focus()
+    }
+  })
+  app
+    .whenReady()
+    .then(createWindow)
+    .then((window) => (win = window))
+}
+
+// create browser window
 function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
@@ -13,18 +38,21 @@ function createWindow() {
 
   // win.maximize()
   Menu.setApplicationMenu(null)
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // win.loadURL('https://test-vshow.fooww.com/group/')
   // win.loadURL('http://192.168.1.108:8081/group/')
   win.loadURL('https://beta-vshow.fooww.com/group-electron/')
+  return win
 }
 
-app.whenReady().then(createWindow)
-
+// register event
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  }
+  if (currentLoginUserId && currentLoginId) {
+    axios.get('http://localhost:8001?loginId=' + currentLoginId) // send request
   }
 })
 
@@ -45,4 +73,9 @@ ipcMain.on('openPC', (event, arg) => {
 ipcMain.on('getMAC', (event) => {
   let interface = networkInterfaces()
   event.reply('replyMAC', interface)
+})
+
+ipcMain.on('login-success', (e, arg) => {
+  currentLoginUserId = arg.userId
+  currentLoginId = arg.loginId
 })
